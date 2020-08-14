1d9bc35920c4d66d96e12e52274c43475e54ae65
unknown <artm-marych@rambler.ru>
2020-08-15 01:28:31 +0200

    indexChange

--- index.php
+++ index.php
@@ -1,19 +1,47 @@
-<?php
+<?
 
-if($_SERVER['REQUEST_URI'] == '/') $page = 'home';
-else{
-    $page = substr ($_SERVER['REQUEST_URI'], 1);
-    if(!preg_match('/^[A-z0-9]{3,15}$/', $page)) exit('error url');
+if ( $_SERVER['REQUEST_URI'] == '/' ) {
+$page = 'home';
+}else {
+ $page = substr($_SERVER['REQUEST_URI'],1);
+ if ( !preg_match('/^[A-z0-9]{3,15}$/', $page) ) exit('error url');
 }
 
 session_start();
 
-if( file_exists('all/'.$page.'.php')) include 'all/'.$page.'.php';
+if ( file_exists('all/'.$page.'.php') ) include 'all/'.$page.'.php';
 
-else if( $_SESSION['ulogin'] == 1 and file_exists('auth/'.$page.'.php')) include 'auth/'.$page.'.php';
+else if ( $_SESSON['ulogin'] == 1 and file_exists('auth/'.$page.'.php') ) include 'auth/'.$page.'.php';
 
-else if( $_SESSION['ulogin'] != 1 and file_exists('guest/'.$page.'.php')) include 'guest/'.$page.'.php';
+else if ( $_SESSON['ulogin'] != 1 and file_exists('guest/'.$page.'.php') ) include 'guest/'.$page.'.php';
 
-else exit('Страница 404');
+function top( $title ){
+    echo '<!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>'.$title.'</title>
+    <link rel="stylesheet" href="style.css"
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    </head>
+    
+    <body>
+    <div class="wrapper">
+    <div class="menu">
+    <a href="/">Главная</a>
+    <a href="login">Вход</a>
+    <a href="/register">Регистрация</a>
+    </div>
 
+    <div class="content">
+    <div class="block">
+    ';
+}
+
+function bottom(){
+    echo '
+    </div>
+    </body>
+    </html>';
+}
 ?>
